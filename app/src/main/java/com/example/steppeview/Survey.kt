8117c4a8590517ecc8705b1e0ff03e447321ca57
bertrand9819 <kotyboame@gmail.com>
2023-08-03 16:14:38 +0200

    ajout du formulaire de succès de fin du process

--- app/src/main/java/com/example/steppeview/Survey.kt
+++ app/src/main/java/com/example/steppeview/Survey.kt
@@ -1,5 +1,4 @@
 
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -27,6 +26,7 @@ import com.example.steppeview.R
 import kotlinx.coroutines.launch
 
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuestionnaireModalBottomSheet(
@@ -45,15 +45,18 @@ fun QuestionnaireModalBottomSheet(
             Column(
                 modifier = Modifier.fillMaxWidth().padding(16.dp)
             ) {
-                StepBar(currentStep = currentStep, totalSteps = totalSteps)
-
-                formSteps.getOrNull(currentStep)?.invoke {
-                    if (currentStep < formSteps.size - 1) {
-                        currentStep += 1
-                    } else {
-
-                        showSuccessMessage = true
+                if (!showSuccessMessage) {
+                    StepBar(currentStep = currentStep, totalSteps = totalSteps)
+                    formSteps.getOrNull(currentStep)?.invoke {
+                        if (currentStep < formSteps.size - 1) {
+                            currentStep += 1
+                        } else {
+                            showSuccessMessage = true
+                        }
                     }
+                } else {
+                    // Show the success bottom sheet here
+                    SuccessMessageForm(onDismiss = { showSuccessMessage = false })
                 }
             }
         }
@@ -76,21 +79,50 @@ fun QuestionnaireModalBottomSheet(
             }
         }
     }
-
-    if (showSuccessMessage) {
-        SuccessMessageForm(onDismiss = { showSuccessMessage = false })
-    }
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuccessMessageBottomSheet(onDismiss: () -> Unit) {
+    BottomSheetScaffold(
+        sheetContent = {
+            Box(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                /*Image(
+                    painter = painterResource(R.drawable.love_104_green), // Replace with your image resource
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.TopCenter)
+                )*/
 
+                Column(
+                    modifier = Modifier.align(Alignment.Center),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Success! Thank you for your rating.")
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            onDismiss()
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "Close")
+                    }
+                }
+            }
+        }
+    ) {}
+}
 
 
 
 @Composable
 fun StepThreeContent(onNextStep: () -> Unit, totalSteps: Int) {
     var selectedEmoji by remember { mutableStateOf("") }
-    var showSuccessMessage by remember { mutableStateOf(false) }
 
-    /*StepBar(currentStep = 3, totalSteps = totalSteps)*/
     Column(
         modifier = Modifier.fillMaxWidth().padding(16.dp)
     ) {
@@ -98,58 +130,40 @@ fun StepThreeContent(onNextStep: () -> Unit, totalSteps: Int) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = "Quelle est votre satisfaction  globale \n" +
-                    "         à l'égard de nos service ?", fontStyle = FontStyle.Normal
+            Text(
+                text = "Quelle est votre satisfaction  globale \n" +
+                        "         à l'égard de nos service ?",
+                fontStyle = FontStyle.Normal
             )
             Spacer(modifier = Modifier.width(30.dp))
-
         }
 
-
-        if (!showSuccessMessage) {
-            EmojiList(onEmojiSelected = { selectedEmoji = it })
-        }
+        EmojiList(onEmojiSelected = { selectedEmoji = it })
 
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            if (!showSuccessMessage) {
-                Button(
-                    onClick = {
-                        if (selectedEmoji.isNotEmpty()) {
-                            onNextStep()
-                        }
-                        if (selectedEmoji.isNotEmpty() && totalSteps == 3) {
-                            showSuccessMessage = true
-                        }
-                    },
-                    enabled = selectedEmoji.isNotEmpty(),
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Next")
-                }
+            Button(
+                onClick = {
+                    if (selectedEmoji.isNotEmpty()) {
+                        onNextStep()
+                    }
+                },
+                enabled = selectedEmoji.isNotEmpty(),
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Next")
             }
-        }
-
-        if (showSuccessMessage) {
-            SuccessMessageForm(onDismiss = { showSuccessMessage = false }) // Show the success message if needed
         }
     }
 }
 
-
-
-
-
-
 @Composable
 fun StepOneContent(onNextStep: () -> Unit, totalSteps: Int) {
     var selectedOption by remember { mutableStateOf("") }
-    var showSuccessMessage by remember { mutableStateOf(false) }
-   /* StepBar(currentStep = 1, totalSteps = totalSteps)*/
 
     Column(
         modifier = Modifier.fillMaxWidth().padding(16.dp)
@@ -161,37 +175,26 @@ fun StepOneContent(onNextStep: () -> Unit, totalSteps: Int) {
             Text(text = "veuillez choisir ?")
         }
 
-        if (!showSuccessMessage) {
-            CheckboxList(onOptionSelected = { selectedOption = it })
-        }
+        CheckboxList(onOptionSelected = { selectedOption = it })
 
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            if (!showSuccessMessage) {
-                Button(
-                    onClick = {
-                        // Validate input here if needed
-                        if (selectedOption.isNotEmpty()) {
-                            onNextStep()
-                        }
-                        if (selectedOption.isNotEmpty() && totalSteps == 3) {
-                            showSuccessMessage = true // Show the success message when the button is clicked
-                        }
-                    },
-                    enabled = selectedOption.isNotEmpty(),
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Next")
-                }
+            Button(
+                onClick = {
+                    // Validate input here if needed
+                    if (selectedOption.isNotEmpty()) {
+                        onNextStep()
+                    }
+                },
+                enabled = selectedOption.isNotEmpty(),
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Next")
             }
-        }
-
-        if (showSuccessMessage) {
-            SuccessMessageForm(onDismiss = { showSuccessMessage = false }) // Show the success message if needed
         }
     }
 }
@@ -199,100 +202,92 @@ fun StepOneContent(onNextStep: () -> Unit, totalSteps: Int) {
 @Composable
 fun StepTwoContent(onNextStep: () -> Unit, totalSteps: Int) {
     var textValue by remember { mutableStateOf("") }
-    var showSuccessMessage by remember { mutableStateOf(false) }
+
     val maxWordCount = 500
 
-    /*StepBar(currentStep = 2, totalSteps = totalSteps)*/
     Column(
         modifier = Modifier.fillMaxWidth().padding(16.dp)
     ) {
         Text(text = "Step 2: Enter your text")
 
-        if (!showSuccessMessage) {
-            var currentWordCount by remember { mutableStateOf(textValue.split("\\s+".toRegex()).size) }
+        var currentWordCount by remember { mutableStateOf(textValue.split("\\s+".toRegex()).size) }
 
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 16.dp),
-                shape = RoundedCornerShape(8.dp),
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            shape = RoundedCornerShape(8.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
             ) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    TextField(
-                        value = textValue,
-                        onValueChange = {
-                            textValue = it
-                            currentWordCount = it.split("\\s+".toRegex()).size
-                        },
-                        maxLines = Int.MAX_VALUE,
-                        textStyle = TextStyle(fontSize = 16.sp),
-                        shape = RoundedCornerShape(8.dp), // Set the shape of the TextField to RoundedCornerShape
-                        colors = TextFieldDefaults.colors(disabledContainerColor = Color.Transparent, focusedIndicatorColor = Color.Transparent, unfocusedIndicatorColor = Color.Transparent)
-                    )
-
-                    Spacer(modifier = Modifier.height(8.dp))
-                }
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End
-            ) {
-                Text(
-                    text = "Mots: $currentWordCount/$maxWordCount",
-                    color = if (currentWordCount <= maxWordCount) Color.Gray else Color.Red,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-            }
-
-            if (!showSuccessMessage) {
-                Button(
-                    onClick = {
-                        if (textValue.isNotBlank()) {
-                            onNextStep()
-                        }
-                        if (textValue.isNotBlank() && totalSteps == 3) {
-                            showSuccessMessage = true
-                        }
+                TextField(
+                    value = textValue,
+                    onValueChange = {
+                        textValue = it
+                        currentWordCount = it.split("\\s+".toRegex()).size
                     },
-                    enabled = textValue.isNotBlank(),
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Next")
-                }
+                    maxLines = Int.MAX_VALUE,
+                    textStyle = TextStyle(fontSize = 16.sp),
+                    shape = RoundedCornerShape(8.dp), // Set the shape of the TextField to RoundedCornerShape
+                    colors = TextFieldDefaults.colors(
+                        disabledContainerColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
             }
         }
 
-        if (showSuccessMessage) {
-            SuccessMessageForm(onDismiss = { showSuccessMessage = false }) // Show the success message if needed
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                text = "Mots: $currentWordCount/$maxWordCount",
+                color = if (currentWordCount <= maxWordCount) Color.Gray else Color.Red,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+
+        Button(
+            onClick = {
+                // Validate input here if needed
+                if (textValue.isNotBlank()) {
+                    onNextStep()
+                }
+            },
+            enabled = textValue.isNotBlank(),
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(text = "Next")
         }
     }
 }
 
+
+
+
 @Composable
-fun StepFourContent(onFinish: () -> Unit, totalSteps: Int) {
+fun StepFourContent(onFinish: () -> Unit) {
     var rating by remember { mutableStateOf(0) }
     var showSuccessMessage by remember { mutableStateOf(false) }
-    if (showSuccessMessage) {
 
-        SuccessMessageForm(onDismiss = { showSuccessMessage = false })
-    } else {
-
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Step 4: Rate with stars")
+    if (!showSuccessMessage) {
+        CustomFormContent(
+            title = "Step 4: Rate with stars",
+            buttonText = "Finish",
+            onButtonClicked = {
+                if (rating > 0) {
+                    onFinish()
+                    showSuccessMessage = true
+                }
             }
-            Spacer(modifier = Modifier.size(20.dp))
-
+        ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Center
@@ -307,30 +302,20 @@ fun StepFourContent(onFinish: () -> Unit, totalSteps: Int) {
                 )
                 Spacer(modifier = Modifier.size(16.dp))
             }
-
-            if (!showSuccessMessage) {
-                Button(
-                    onClick = {
-                        if (rating > 0) {
-                            onFinish()
-                            if (totalSteps == 3) {
-                                showSuccessMessage = true
-                            }
-                        }
-                    },
-                    enabled = rating > 0,
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                        .fillMaxWidth()
-                ) {
-                    Text(text = "Finish")
-                }
-            }
         }
+    } else {
+        // Show the success bottom sheet here
+        SuccessMessageForm(onDismiss = { showSuccessMessage = false })
     }
 }
+
 @Composable
-fun SuccessMessageForm(onDismiss: () -> Unit) {
+fun CustomFormContent(
+    title: String,
+    buttonText: String,
+    onButtonClicked: () -> Unit,
+    content: @Composable () -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxWidth().padding(16.dp)
     ) {
@@ -338,19 +323,31 @@ fun SuccessMessageForm(onDismiss: () -> Unit) {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = "Success! Thank you for your rating.")
+            Text(text = title)
         }
+
+        Spacer(modifier = Modifier.size(20.dp))
+
+        content()
+
         Button(
-            onClick = {
-                onDismiss()
-            },
-            modifier = Modifier.padding(top = 16.dp).align(Alignment.CenterHorizontally)
+            onClick = onButtonClicked,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
         ) {
-            Text(text = "Close")
+            Text(text = buttonText)
         }
     }
 }
-
+@Composable
+fun SuccessMessageForm(onDismiss: () -> Unit) {
+    CustomFormContent(
+        title = "Success! Thank you for your rating.",
+        buttonText = "Close",
+        onButtonClicked = onDismiss
+    ) {}
+}
 @Composable
 fun StarRating(
     maxRating: Int = 5,
@@ -546,8 +543,9 @@ fun SurveyForms() {
         { onNextStep -> StepOneContent(onNextStep, totalSteps) },
         { onNextStep -> StepTwoContent(onNextStep, totalSteps) },
         { onNextStep -> StepThreeContent(onNextStep, totalSteps) },
-        { onFinish -> StepFourContent(onFinish, totalSteps) }
+        { onFinish -> StepFourContent(onFinish) }
     )
+
 
     QuestionnaireModalBottomSheet(totalSteps = totalSteps, formSteps = formSteps)
 
